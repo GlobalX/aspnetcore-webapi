1069496c42f0a592f87f0852ce522b3bc292648f
Drew Freyling <drew.freyling@gmail.com>
2020-08-26 13:32:54 +1000

    override all methods for interception

--- webapi/Infrastructure/MultiTenancyDbCommandInterceptor.cs
+++ webapi/Infrastructure/MultiTenancyDbCommandInterceptor.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Data.Common;
 using System.Threading;
 using System.Threading.Tasks;
@@ -14,13 +15,33 @@ namespace webapi.Infrastructure
             _tenantInfo = tenantInfo;
         }
 
+        public override InterceptionResult<DbDataReader> ReaderExecuting(DbCommand command, CommandEventData eventData, InterceptionResult<DbDataReader> result)
+        {
+            command.CommandText = $"SET app.current_tenant = '{_tenantInfo.Id}'; {command.CommandText}";
+            string query = command.CommandText;
+
+            foreach (DbParameter p in command.Parameters)
+            {
+                query = query.Replace(p.ParameterName, p.Value.ToString());
+            }
+            Console.WriteLine(query);
+
+            return base.ReaderExecuting(command, eventData, result);
+        }
+
         public override Task<InterceptionResult<DbDataReader>> ReaderExecutingAsync(DbCommand command,
             CommandEventData eventData,
             InterceptionResult<DbDataReader> result,
             CancellationToken cancellationToken = default)
         {
             command.CommandText = $"SET app.current_tenant = '{_tenantInfo.Id}'; {command.CommandText}";
+            string query = command.CommandText;
 
+            foreach (DbParameter p in command.Parameters)
+            {
+                query = query.Replace(p.ParameterName, p.Value.ToString());
+            }
+            Console.WriteLine(query);
             return base.ReaderExecutingAsync(command, eventData, result, cancellationToken);
         }
 
@@ -28,7 +49,13 @@ namespace webapi.Infrastructure
             CancellationToken cancellationToken = new CancellationToken())
         {
             command.CommandText = $"SET app.current_tenant = '{_tenantInfo.Id}'; {command.CommandText}";
-            
+            string query = command.CommandText;
+
+            foreach (DbParameter p in command.Parameters)
+            {
+                query = query.Replace(p.ParameterName, p.Value.ToString());
+            }
+            Console.WriteLine(query);
             return base.NonQueryExecutingAsync(command, eventData, result, cancellationToken);
         }
 
@@ -36,8 +63,36 @@ namespace webapi.Infrastructure
             CancellationToken cancellationToken = new CancellationToken())
         {
             command.CommandText = $"SET app.current_tenant = '{_tenantInfo.Id}'; {command.CommandText}";
+            Console.WriteLine(command.CommandText);
 
             return base.ScalarExecutingAsync(command, eventData, result, cancellationToken);
+        }
+
+        public override InterceptionResult<object> ScalarExecuting(DbCommand command, CommandEventData eventData, InterceptionResult<object> result)
+        {
+            command.CommandText = $"SET app.current_tenant = '{_tenantInfo.Id}'; {command.CommandText}";
+            string query = command.CommandText;
+
+            foreach (DbParameter p in command.Parameters)
+            {
+                query = query.Replace(p.ParameterName, p.Value.ToString());
+            }
+            Console.WriteLine(query);
+            return base.ScalarExecuting(command, eventData, result);
+        }
+
+        public override InterceptionResult<int> NonQueryExecuting(DbCommand command, CommandEventData eventData, InterceptionResult<int> result)
+        {
+            command.CommandText = $"SET app.current_tenant = '{_tenantInfo.Id}'; {command.CommandText}";
+
+            string query = command.CommandText;
+
+            foreach (DbParameter p in command.Parameters)
+            {
+                query = query.Replace(p.ParameterName, p.Value.ToString());
+            }
+            Console.WriteLine(query);
+            return base.NonQueryExecuting(command, eventData, result);
         }
     }
 }
