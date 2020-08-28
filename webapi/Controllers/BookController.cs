d5e4e17aed86ee3cad8661c471474918a65770d8
Tom Shirley <tom.shirley@globalx.com.au>
2020-08-28 12:11:17 +1000

    feat: implement getById, insert, delete book repository methods

--- webapi/Controllers/BookController.cs
+++ webapi/Controllers/BookController.cs
@@ -5,6 +5,9 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
 using webapi.Models;
 using webapi.Repositories;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Linq.Expressions;
 
 namespace webapi.Controllers
 {
@@ -20,7 +23,12 @@ namespace webapi.Controllers
 
         [HttpGet]
         [Route("")]
-        public IEnumerable<Book> GetAllBooks() => _bookRepository.GetAll();
+        public IEnumerable<Book> GetAllBooks()
+        {
+            var results = _bookRepository.GetAll();
+
+            return new List<Book>() { results.First() };
+        }
 
         [HttpGet]
         [Route("{bookId}")]
@@ -29,9 +37,12 @@ namespace webapi.Controllers
         [HttpPost]
         [Route("")]
         [AllowAnonymous]
-        public void AddBook([FromBody] Book book)
+        public object AddBook([FromBody] Book book)
         {
+            book.Id = Guid.NewGuid();
             _bookRepository.Insert(book);
+
+            return new { Id = book.Id };
         }
 
         [HttpDelete]
--- webapi/Repositories/BookRepository.cs
+++ webapi/Repositories/BookRepository.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Linq;
 using Dapper;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
 using Npgsql;
 using webapi.Infrastructure;
 using webapi.Models;
@@ -34,12 +36,35 @@ namespace webapi.Repositories
 
         public Book GetById(Guid id)
         {
-            throw new NotImplementedException();
+            const string sql = "SELECT \"Id\", \"CreatedAt\", \"Title\", \"Year\", \"AuthorId\", \"TenantId\" FROM trust_views.books_vw WHERE \"Id\" = @Id;";
+            var parameters = new { Id = id };
+
+            using (var connection = _createConnection())
+            {
+                connection.Open();
+
+                var book = connection.Query<Book>(sql, parameters).FirstOrDefault();
+                connection.Close();
+
+                return book;
+            }
         }
 
         public void Insert(Book entity)
         {
-            throw new NotImplementedException();
+            const string sql = "INSERT INTO trust_views.books_vw(\"Id\", \"TenantId\", \"Title\", \"Year\", \"CreatedAt\", \"AuthorId\")" +
+                               "VALUES(@Id, @TenantId, @Title, @Year, current_timestamp, @AuthorId);";
+            var parameters = new { Id = entity.Id, TenantId = entity.TenantId, Title = entity.Title, Year = entity.Year, AuthorId = entity.AuthorId };
+
+            using (var connection = _createConnection())
+            {
+                connection.Open();
+
+                var book = connection.Execute(sql, parameters);
+                connection.Close();
+
+                return;
+            }
         }
 
         public void Update(Book entity)
@@ -49,7 +74,18 @@ namespace webapi.Repositories
 
         public void Delete(Guid id)
         {
-            throw new NotImplementedException();
+            const string sql = "DELETE FROM trust_views.books_vw WHERE \"Id\" = @Id;";
+            var parameters = new { Id = id };
+
+            using (var connection = _createConnection())
+            {
+                connection.Open();
+
+                var book = connection.Execute(sql, parameters);
+                connection.Close();
+
+                return;
+            }
         }
     }
 }
